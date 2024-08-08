ff14e272e4c4089bd340d36f7746a853a72a47b8
adamalcantara <alcantaradevelopment@gmail.com>
2024-08-07 23:09:50 -0400

    Cleared some code, added an array to state

--- src/Content.js
+++ src/Content.js
@@ -1,37 +1,27 @@
 import React, { useState } from 'react';
 
 const Content = () => {
-    const [name, setName] = useState('Dave');
-    const [count, setCount] = useState(0);
-
-    const handleNameChange = () => {
-        // array of names
-        const names = ['Bob', 'Adam', 'Dave', 'Kevin'];
-
-        // get a random number between 0 and 3
-        const int = Math.floor(Math.random() * 4);
-
-        // return the nth number (int) from names and set the name state to it
-        setName(names[int]);
-    }
-
-    const handleClick = () => {
-        setCount(count + 1);
-        setCount(count + 1);
-        console.log(count);
-    }
-    const handleClick2 = (name) => {
-        console.log(count);
-    }
+    const [items, setItems] = useState([
+        {
+            id: 1,
+            checked: false,
+            item: "One half pound bag of Cocoa Covered Almonds Unsalted"
+        },
+        {
+            id: 2,
+            checked: false,
+            item: "Item 2"
+        },
+        {
+            id: 3,
+            checked: false,
+            item: "Item 3"
+        },
+    ]);
 
     return (
         <main>
-            <p onDoubleClick={handleClick}>
-                Hello {name}!
-            </p>
-            <button onClick={handleNameChange}>Change Name</button>
-            <button onClick={handleClick}>Click It</button>
-            <button onClick={handleClick2}>Click It</button>
+           
         </main>
     )
 }
