466fff6ffc2202017a61bdbf82bc1b660ef6f433
adamalcantara <alcantaradevelopment@gmail.com>
2024-08-10 21:19:11 -0400

    ItemList is now its own component

--- src/Content.js
+++ src/Content.js
@@ -1,40 +1,20 @@
 import React from 'react';
-import { FaTrashAlt } from 'react-icons/fa';
+import ItemList from './ItemList';
 
-const Content = ({ items, handleCheck, handleDelete}) => {
+const Content = ({ items, handleCheck, handleDelete }) => {
 
     return (
         <main>
             {items.length ? (
-                <ul>
-                    {/* Map over the items and display them on the page as indicated */}
-                    {items.map((item) => (
-                        <li className='item' key={item.id}>
-                            <input
-                                type="checkbox"
-                                onChange={() => handleCheck(item.id)}
-                                checked={item.checked}
-                            />
-                            <label
-                                // Check the item on double click
-                                onDoubleClick={() => handleCheck(item.id)}
-
-                                // strike through the item if it is checked
-                                style={(item.checked) ? {textDecoration: 'line-through'} : null}
-                            >{item.item}</label>
-                            <FaTrashAlt 
-                                // run delete function on click
-                                onClick={() => handleDelete(item.id)}
-                                role='button' 
-                                tabIndex='0' 
-                            />
-                        </li>
-                    ))}
-                </ul>
+                <ItemList
+                    items={items}
+                    handleCheck={handleCheck}
+                    handleDelete={handleDelete}
+                />
             ) : (
                 // Alternate code to display if there is not anything in the list
                 <p style={{ marginTop: '2rem' }}>Your list is empty.</p>
-            ) }
+            )}
         </main>
     )
 }
--- src/Footer.js
+++ src/Footer.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Footer = ({ length }) => {
   return (
     <footer>
+      {/* Utilize length from props, use a ternary to determine singular or plural */}
         <p>{length} List {length === 1 ? "Item" : "Items"}</p>
     </footer>
   )
